e3edf925630437eb883fd7cc7b58f90c82f48119
Katshual <139873320+Katshual@users.noreply.github.com>
2025-01-27 23:59:28 +0100

    création du dossier backEndDevelopment et 6 sous-dossiers

---
+++ backEndDevelopment/backendJSWithNodeJS/runningAProgramWithNode/app.js
@@ -0,0 +1,9 @@
+let noun1 = "creature";
+let adjective = "silly";
+let noun2 = "dog";
+let verb = "pee";
+let noun3 = "flowers";
+
+console.log(
+  `The world's first ${noun1} was a very ${adjective} ${noun2} who loved to ${verb} while eating ${noun3} for every meal.`
+);
